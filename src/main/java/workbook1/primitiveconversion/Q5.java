4e6f46951adf63d9bde774ad6ff6cad35d7a4996
cjn404 <moi404@naver.com>
2025-08-05 22:12:29 +0900

    feat: [primitiveconversion] Q5 구현

--- src/main/java/workbook1/primitiveconversion/Q5.java
+++ src/main/java/workbook1/primitiveconversion/Q5.java
@@ -9,5 +9,13 @@ public class Q5 {
         double y = sc.nextDouble();
         double z = sc.nextDouble();
         // TODO: x, y, z를 int로 변환 후 평균을 float으로 출력
+        // x, y, z를 int로 변환
+        int xInt = (int) x;
+        int yInt = (int) y;
+        int zInt = (int) z;
+        // 평균을 float으로
+        float average = (float) (xInt + yInt + zInt) / 3;
+        // 출력
+        System.out.println(average);
     }
 }
