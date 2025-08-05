175997edeb248d819251b6cff905f2c042c5c656
cjn404 <moi404@naver.com>
2025-08-05 22:21:44 +0900

    style: [primitiveconversion] Q1~Q6 개행 추가

--- src/main/java/workbook1/primitiveconversion/Q4.java
+++ src/main/java/workbook1/primitiveconversion/Q4.java
@@ -8,10 +8,13 @@ public class Q4 {
         float f1 = sc.nextFloat();
         float f2 = sc.nextFloat();
         // TODO: f1을 int로 강제 변환, f2를 double로 자동 변환해 합산 후 출력
+
         // f1을 int로 강제 변환
         int i = (int) f1;
+
         // f2를 double로 자동 변환해 합산
         double sum = i + f2;
+
         // 출력(소수점 첫째자리까지만)
         System.out.printf("%.1f", sum);
     }
--- src/main/java/workbook1/primitiveconversion/Q6.java
+++ src/main/java/workbook1/primitiveconversion/Q6.java
@@ -8,11 +8,14 @@ public class Q6 {
         String s1 = sc.next();
         String s2 = sc.next();
         // TODO: s1, s2를 int로 변환 후 차이를 byte로 변환하여 출력
+
         // s1, s2를 int로 변환
         int s1Int = Integer.parseInt(s1);
         int s2Int = Integer.parseInt(s2);
+
         // 차이를 byte로 변환
         byte sub = (byte) (s1Int - s2Int);
+
         // 출력
         System.out.println(sub);
     }
