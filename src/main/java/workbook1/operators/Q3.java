606c475b5653dce67a1ca1f01d7d3a4a66a71808
H <xellos216@gmail.com>
2025-08-06 04:37:55 +0900

    Solve operators

--- src/main/java/workbook1/operators/Q3.java
+++ src/main/java/workbook1/operators/Q3.java
@@ -7,6 +7,23 @@ public class Q3 {
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble();
         double y = sc.nextDouble();
-        // TODO: x &gt; y 결과와 !(x &gt; y) 결과 출력
+        // TODO: x > y 결과와 !(x < y) 결과 출력
+        if (x > y) {
+            System.out.print(true + " ");
+        } else {
+            System.out.print(false + " ");
+        }
+
+        if (!(x > y)) {
+            System.out.print(true);
+        } else {
+            System.out.print(false);
+        }
+
+//        System.out.println(!(x > y));
+
+//        boolean result = x > y;
+//        System.out.println(result + " " +!result);
+
     }
 }
