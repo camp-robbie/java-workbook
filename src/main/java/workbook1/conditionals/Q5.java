d31a0e30a5aebe58476c8c210beab4a6b3108f5b
cjn404 <moi404@naver.com>
2025-08-06 00:39:13 +0900

    feat: [conditionals] Q5 구현

--- src/main/java/workbook1/conditionals/Q5.java
+++ src/main/java/workbook1/conditionals/Q5.java
@@ -9,5 +9,11 @@ public class Q5 {
         int b = sc.nextInt();
         int c = sc.nextInt();
         // TODO: a, b, c 중 최대값 또는 같은 값 여부 출력
+
+        if (a == b && a == c) {
+            System.out.println("같은 수 존재");
+        } else {
+            System.out.println(Math.max(a, Math.max(b, c)));
+        };
     }
 }
