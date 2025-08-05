de64b99981eeef7033f2483a5dc521570eea4d6f
H <xellos216@gmail.com>
2025-08-06 03:23:30 +0900

    Solve primitiveconversion

--- src/main/java/workbook1/primitiveconversion/Q4.java
+++ src/main/java/workbook1/primitiveconversion/Q4.java
@@ -7,6 +7,11 @@ public class Q4 {
         Scanner sc = new Scanner(System.in);
         float f1 = sc.nextFloat();
         float f2 = sc.nextFloat();
-        // TODO: f1을 int로 강제 변환, f2를 double로 자동 변환해 합산 후 출력
+
+        int a = (int) f1; // cast f1 to int
+
+        System.out.println((double)a + f2); // cast a to double, f2 is promoted to double automatically
+
+      // TODO: f1을 int로 강제 변환, f2를 double로 자동 변환해 합산 후 출력
     }
 }
