f324d9f5aa83d40d263c426b405fff4e42c22df2
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-05 20:18:51 +0900

    primitiveconversion Q2 구현

--- src/main/java/workbook1/primitiveconversion/Q2.java
+++ src/main/java/workbook1/primitiveconversion/Q2.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double d = sc.nextDouble();
-        // TODO: d를 int로 강제 형변환하여 차이를 계산 후 출력
+        double d = sc.nextDouble(); // 입력 값
+
+        double result; // 연산 결과를 담을 변수
+
+        int newNum = (int) d; // double d를 강제로 int 형변환
+
+        result = d-newNum; // (double) result = (double) d - (int) newNum
+
+        System.out.println(result); // result는 double 이기 때문에 결과 값을 소수점까지 표시 가능
     }
 }
