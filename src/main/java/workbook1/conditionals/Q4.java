2684fe455651920795c90240e6618f6d9f2ecd69
eunji-0508 <cojia@naver.com>
2025-08-06 05:23:23 +0900

    feat: [conditionals] Q4 구현

--- src/main/java/workbook1/conditionals/Q4.java
+++ src/main/java/workbook1/conditionals/Q4.java
@@ -6,6 +6,16 @@ public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
+
         // TODO: 윤년 판별 로직 구현
+        // 윤년은 연도가 4의 배수이면서 100의 배수가 아니거나 400의 배수인 해
+
+        if(year % 4 == 0 && year % 100 != 0) {      // year이 4의 배수이면서 100의 배수가 아닐 경우
+            System.out.println("윤년");              // 윤년을 출력함
+        } else if(year % 400 == 0) {                // year이 400의 배수라면
+            System.out.println("윤년");              // 윤년을 출력함
+        } else {                                    // 이외에는
+            System.out.println("윤년이 아닙니다. (평년)");   // 윤년이 아닙니다. (평년)을 출력함
+        }
     }
 }
