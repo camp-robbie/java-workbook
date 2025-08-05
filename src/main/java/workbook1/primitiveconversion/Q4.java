464563101a976902b6d47f69574919967b56cc6a
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-05 20:53:53 +0900

    primitiveconversion Q4 구현

--- src/main/java/workbook1/primitiveconversion/Q4.java
+++ src/main/java/workbook1/primitiveconversion/Q4.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float f1 = sc.nextFloat();
-        float f2 = sc.nextFloat();
-        // TODO: f1을 int로 강제 변환, f2를 double로 자동 변환해 합산 후 출력
+        float f1 = sc.nextFloat(); // 첫번째 입력 값
+        float f2 = sc.nextFloat(); // 두번째 입력 값
+
+        int num1 = (int)f1; // int로 강제 변환
+
+        double num2 = f2; // double로 자동 형변환
+
+        double sum = num1 + num2; // 연산 결과를 sum에 저장
+
+        System.out.printf("%.1f",sum); // printf()을 활용하여 불필요한 소숫점 제거
     }
 }
