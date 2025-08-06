e76ec4fabc6c09971962ff2e105467016007c450
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-06 20:50:07 +0900

    feat: [loops] Q3 구현

--- src/main/java/workbook1/loops/Q3.java
+++ src/main/java/workbook1/loops/Q3.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 
 public class Q3 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        // TODO: do-while 문으로 3의 배수의 합 계산 후 출력
+
+        // 반복문에 사용할 변수를 선언합니다.
+        // i 는 인덱스, sum은 총 합계를 뜻합니다.
+        int i = 0;
+        int sum = 0;
+
+        // do-while 문입니다.
+        // 장점 -> 최소 한 번은 실행된다.
+        do {
+            if(i % 3 == 0){
+                sum += i;
+            }
+            i++;
+        }while (i<=n);
+
+        // 출력합니다.
+        System.out.println(sum);
     }
 }
