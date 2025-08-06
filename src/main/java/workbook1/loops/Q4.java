3b4c673712c14eba94f24d396a839384420be7d5
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-06 20:50:17 +0900

    feat: [loops] Q4 구현

--- src/main/java/workbook1/loops/Q4.java
+++ src/main/java/workbook1/loops/Q4.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 
 public class Q4 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int m = sc.nextInt();
         int n = sc.nextInt();
-        // TODO: 이중 for 문으로 별 패턴 출력
+
+        // 2중 for 문입니다.
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                // 같은 라인에 찍혀져야 하므로, println 이 아니라
+                // 그냥 print를 사용합니다. (같은 줄에 출력)
+                System.out.print("*");
+            }
+            // 개행을 위한 println 입니다.
+            System.out.println();
+        }
     }
 }
