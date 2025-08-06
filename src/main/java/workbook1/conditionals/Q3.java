b2198b77c4d7a235cc68deecf259f0b7717dade4
grace-jsy <sseo.oee@gmail.com>
2025-08-06 17:17:28 +0900

    feat: [conditionals] Q3 solved

--- src/main/java/workbook1/conditionals/Q3.java
+++ src/main/java/workbook1/conditionals/Q3.java
@@ -9,5 +9,29 @@ public class Q3 {
         int s2 = sc.nextInt();
         int s3 = sc.nextInt();
         // TODO: 평균을 계산하고 중첩 if-else로 학점 출력
+
+        // 90점 이상 : A
+        // 80점 이상 : B
+        // 70점 이상 : C
+        // 60점 이상 : D
+        // 그 외 : F
+
+        int average = (s1 + s2 + s3) / 3;
+
+        // 평균이 90 이상일 때를 먼저 확인하는 if문 작성
+        if (average >= 90) {
+            System.out.println(average + " = A");
+        } else if (average >= 80) {
+            System.out.println(average + " = B");
+        } else if (average >= 70) {
+            System.out.println(average + " = C");
+        } else if (average >= 60) {
+            System.out.println(average + " = D");
+        } else {
+            System.out.println(average + " = F");
+        }
+
+
+
     }
 }
