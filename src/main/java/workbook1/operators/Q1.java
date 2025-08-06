0be5fe88f63bf996d188e698bf4b74534ee5235a
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-06 10:06:27 +0900

    operators Q1 구현

--- src/main/java/workbook1/operators/Q1.java
+++ src/main/java/workbook1/operators/Q1.java
@@ -7,6 +7,12 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        // TODO: 산술 연산자(+, -, *, /, %) 결과 출력
+        // 산술 연산자(+, -, *, /, %) 결과 출력
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int mod = a % b;
+        System.out.print(sum + " " + diff + " " + mul + " " + div + " " + mod); // 연산 결과를 한줄로 출력
     }
 }
