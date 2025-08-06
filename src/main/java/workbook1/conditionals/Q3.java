7aa4cb6b1182de136ee370a8ad885c205d039e27
HY-WG <mung94c@gmail.com>
2025-08-06 11:57:10 +0900

    조건문과 중첩 조건문

--- src/main/java/workbook1/conditionals/Q3.java
+++ src/main/java/workbook1/conditionals/Q3.java
@@ -9,5 +9,39 @@ public class Q3 {
         int s2 = sc.nextInt();
         int s3 = sc.nextInt();
         // TODO: 평균을 계산하고 중첩 if-else로 학점 출력
+
+        int average = (s1 + s2 + s3) / 3;
+
+        if (average > 90) {
+            System.out.println("A");
+        } else if (average > 80) {
+            System.out.println("B");
+        } else if (average > 70) {
+            System.out.println("C");
+        } else if (average > 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
+// 중첩 else-if  구조도 연습해보자
+        int average2 = (s1 + s2 + s3) / 3;
+        if (average2 > 60) {
+            if (average2 > 70) {
+                if (average2 > 80) {
+                    if (average2 > 90) {
+                        System.out.println("A");
+                    } else {
+                        System.out.println("B");
+                    }
+                } else {
+                    System.out.println("C");
+                }
+            } else {
+                System.out.println("D");
+            }
+        } else {
+            System.out.println("F");
+        }
     }
 }
