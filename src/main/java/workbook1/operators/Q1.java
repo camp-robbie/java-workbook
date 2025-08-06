286fb73764f3d74b4e55300b5c155959ed7f2c5f
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-06 16:13:33 +0900

    feat: [operators] Q1 구현

--- src/main/java/workbook1/operators/Q1.java
+++ src/main/java/workbook1/operators/Q1.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 
 public class Q1 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int a = sc.nextInt();
         int b = sc.nextInt();
-        // TODO: 산술 연산자(+, -, *, /, %) 결과 출력
+
+        // 두 개의 정수를 입력받아 산술 연산자를 모두 적용한 결과를 한 줄에 출력합니다.
+        // 한 줄에 출력하고, 사이에 띄어쓰기를 주어 값을 구분하기 위해 String 값과의
+        // 연산을 실행하여, 연산 이후에는 String 값으로 처리되게끔 했습니다.
+        System.out.println((a+b)+" "+(a-b)+" "+(a*b)+" "+(a/b)+" "+(a%b));
     }
 }
