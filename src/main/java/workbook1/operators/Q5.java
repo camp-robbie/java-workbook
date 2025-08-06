bac5739b57a2337f6fe1e7cc701fda560a54babe
grace-jsy <sseo.oee@gmail.com>
2025-08-06 17:48:34 +0900

    xx

--- src/main/java/workbook1/operators/Q5.java
+++ src/main/java/workbook1/operators/Q5.java
@@ -9,5 +9,14 @@ public class Q5 {
         int b = sc.nextInt();
         int c = sc.nextInt();
         // TODO: a += b * c 연산 후 a, b, c 출력
+<<<<<<< Updated upstream
+=======
+        // a = (a + b) * c
+
+
+
+
+
+>>>>>>> Stashed changes
     }
 }
