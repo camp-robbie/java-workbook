dac40a69e29d09452cd270afb251be4b7f0b6abc
grace-jsy <sseo.oee@gmail.com>
2025-08-06 09:58:36 +0900

    feat: [primitiveconversion] Q2 add scanner

--- src/main/java/workbook1/primitiveconversion/Q3.java
+++ src/main/java/workbook1/primitiveconversion/Q3.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 숫자를 입력 받기
+        System.out.println("b: ");
         byte b = sc.nextByte();
+        System.out.println("s: ");
         short s = sc.nextShort();
+        System.out.println("i: ");
         int i = sc.nextInt();
+        System.out.println("j: ");
         long l = sc.nextLong();
         // TODO: b, s, i, l을 long으로 변환해 합산 후 float으로 변환하여 출력
+
     }
 }
