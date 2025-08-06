36b961e5db22ce576ddae47f0c91a87611407816
grace-jsy <sseo.oee@gmail.com>
2025-08-06 09:53:34 +0900

    feat: [primitiveconversion] Q2 solved

--- src/main/java/workbook1/primitiveconversion/Q2.java
+++ src/main/java/workbook1/primitiveconversion/Q2.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double d = sc.nextDouble();
+        System.out.print("doubleNumber = ");
+        double doubleNumber = sc.nextDouble();
         // TODO: d를 int로 강제 형변환하여 차이를 계산 후 출력
+
+        // int로 강제 형변환
+        int intNumber = (int) doubleNumber;
+        System.out.println("intNumber = " + intNumber);
+
+        // 차이를 계산 후 출력
+        double result = doubleNumber - intNumber;
+        System.out.println("result = " + result);
+        
+        
+
     }
 }
