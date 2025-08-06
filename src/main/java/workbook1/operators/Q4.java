fab7c5061a64d18d7322536b83da61f4f32c0173
grace-jsy <sseo.oee@gmail.com>
2025-08-06 16:34:53 +0900

    feat: [operators] Q4 solved

--- src/main/java/workbook1/operators/Q4.java
+++ src/main/java/workbook1/operators/Q4.java
@@ -9,5 +9,13 @@ public class Q4 {
         int b = sc.nextInt();
         int c = sc.nextInt();
         // TODO: b가 a보다 크고 c보다 작은지 평가하여 출력
+
+        // b가 a 보다 크고 = b > a
+        // b가 c 보다 작은지 =  b < c
+        // 둘 다 만족해야 하기 때문에 && 논리 연산자 사용
+
+        boolean result = b > a && b < c;
+        System.out.println("result = " + result);
+
     }
 }
