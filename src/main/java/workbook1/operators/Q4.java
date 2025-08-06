645c991a7325305d87a79b91e14f57fd9a25feab
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-06 16:14:07 +0900

    feat: [operators] Q4 구현

--- src/main/java/workbook1/operators/Q4.java
+++ src/main/java/workbook1/operators/Q4.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 
 public class Q4 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        // TODO: b가 a보다 크고 c보다 작은지 평가하여 출력
+
+        // 비교를 두 번 해야 합니다.
+        // b가 a보다 큰 동시에 c보다 작아야 합니다.
+        // && 연산자는 두 개의 boolean 값이 참일 때는 참,
+        // 아닐 경우(0 0, 0 1, 1 0) 에는 거짓을 반환합니다.
+        System.out.println(a<b && b<c);
     }
 }
