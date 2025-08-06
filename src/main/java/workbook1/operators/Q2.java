940ead87728aa03b0c11fce465a2f88a8e87d5e1
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-06 13:43:04 +0900

    operators Q2 구현

--- src/main/java/workbook1/operators/Q2.java
+++ src/main/java/workbook1/operators/Q2.java
@@ -7,5 +7,11 @@ public class Q2 {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         // TODO: 전위 증가와 후위 증가 결과 출력
+
+        int firstNum = ++num;     // 전위 증가 결과를 저장하는 변수
+        int secondNum = num++;    // 후위 증가 결과를 저장하는 변수
+
+        System.out.println(firstNum + " " + secondNum);    // 전위 증가 결과와 후위 증가 결과를 출력하여 결과를 확인함
+        sc.close(); // 자원 누수 방지
     }
 }
