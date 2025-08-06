91f78f509a07317a3ef72be8ea3bfc948c42bea5
zerone <bazz0324@naver.com>
2025-08-06 13:46:46 +0900

    "feat: [primitiveconversion] Q3 구현"

--- src/main/java/workbook1/primitiveconversion/Q3.java
+++ src/main/java/workbook1/primitiveconversion/Q3.java
@@ -9,6 +9,22 @@ public class Q3 {
         short s = sc.nextShort();
         int i = sc.nextInt();
         long l = sc.nextLong();
+
         // TODO: b, s, i, l을 long으로 변환해 합산 후 float으로 변환하여 출력
+
+        long lb = b; // long 으로 자동 형변환
+        long ls = s; // long 으로 자동 형변환
+        long li = i; // long 으로 자동 형변환
+        long sum = lb + ls + li + l; // 합산
+
+        float result = sum; // float으로 자동 형변환
+
+        // 출력
+//        System.out.println("lb = " + lb);
+//        System.out.println("ls = " + ls);
+//        System.out.println("li = " + li);
+//        System.out.println("l = " + l);
+//        System.out.println("sum = " + sum);
+        System.out.println(result);
     }
 }
