568dc43dfc092f4b57da861cb74b38d9ea9cfa04
leetwin1115 <jjeepp1115@gmail.com>
2025-08-06 10:15:35 +0900

    primitiveconversion-park-jungeun완료

--- src/main/java/workbook1/primitiveconversion/Q2.java
+++ src/main/java/workbook1/primitiveconversion/Q2.java
@@ -5,7 +5,15 @@ import java.util.Scanner;
 public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("실수형 숫자를 입력하세요 : ");
         double d = sc.nextDouble();
         // TODO: d를 int로 강제 형변환하여 차이를 계산 후 출력
+
+        //double->int 큰 자료형->작은 자료형 강제형변환
+        int d_num = (int)d;
+        double a_num = d - d_num;
+        System.out.println("강제 형변환 후의 값 : " + d_num);
+        System.out.printf("원래 %.1f의 차이 : ", a_num);
     }
 }
--- src/main/java/workbook1/primitiveconversion/Q4.java
+++ src/main/java/workbook1/primitiveconversion/Q4.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("첫 번째 실수형 숫자를 입력해주세요 : ");
         float f1 = sc.nextFloat();
+        System.out.println("두 번째 실수형 숫자를 입력해주세요 : ");
         float f2 = sc.nextFloat();
         // TODO: f1을 int로 강제 변환, f2를 double로 자동 변환해 합산 후 출력
+
+        //4byte 정수형타입 -> 4byte 실수형타입
+        int f1_num = (int)f1;
+        //4byte 실수형타입 -> 8byte 실수형타입
+        double f2_num = f2;
+
+        double sum = f1_num + f2_num;
+        System.out.printf("두 값의 합 : %.1f", sum);
     }
 }
