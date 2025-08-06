357cc7f9dc9cd5ff5b9280302e7fbd655c9e855d
grace-jsy <sseo.oee@gmail.com>
2025-08-06 17:32:59 +0900

    feat: [conditionals] Q5 solved

--- src/main/java/workbook1/conditionals/Q5.java
+++ src/main/java/workbook1/conditionals/Q5.java
@@ -9,5 +9,17 @@ public class Q5 {
         int b = sc.nextInt();
         int c = sc.nextInt();
         // TODO: a, b, c 중 최대값 또는 같은 값 여부 출력
+
+        if  (a == b && a == c && b == c) {
+            System.out.println("All three numbers are the same");
+        } else {
+            if (a > b && a > c) {
+                System.out.println("A is the biggest number");
+            } else if (b > a && b > c) {
+                System.out.println("B is the biggest number");
+            } else if (c > a && a > b) {
+                System.out.println("C is the biggest number");
+            }
+        }
     }
 }
