dea2b1691da49ab58d843d566f21baf3c3b46d55
leetwin1115 <jjeepp1115@gmail.com>
2025-08-06 11:45:40 +0900

    conditionals-park-jungeun완료

--- src/main/java/workbook1/conditionals/Q2.java
+++ src/main/java/workbook1/conditionals/Q2.java
@@ -5,7 +5,35 @@ import java.util.Scanner;
 public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("1~7중에 숫자를 입력하세요. 해당 숫자에 맞는 요일을 출력합니다.");
         int day = sc.nextInt();
         // TODO: switch 문을 이용하여 요일 출력
+
+        switch (day) {
+            case 1:
+                System.out.println("월요일입니다. 입력하신 숫자 : "+day);
+                break;
+            case 2:
+                System.out.println("화요일입니다. 입력하신 숫자 : "+day);
+                break;
+            case 3:
+                System.out.println("수요일입니다. 입력하신 숫자 : "+day);
+                break;
+            case 4:
+                System.out.println("목요일입니다. 입력하신 숫자 : "+day);
+                break;
+            case 5:
+                System.out.println("금요일입니다. 입력하신 숫자 : "+day);
+                break;
+            case 6:
+                System.out.println("토요일입니다. 입력하신 숫자 : "+day);
+                break;
+            case 7:
+                System.out.println("일요일입니다. 입력하신 숫자 : "+day);
+                break;
+            default:
+                System.out.println("[잘못된입력] 1~7까지중에 입력하세요 휴먼");
+        }
     }
 }
--- src/main/java/workbook1/conditionals/Q5.java
+++ src/main/java/workbook1/conditionals/Q5.java
@@ -5,9 +5,25 @@ import java.util.Scanner;
 public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요.");
         int a = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요.");
         int b = sc.nextInt();
+        System.out.println("세 번째 숫자를 입력하세요.");
         int c = sc.nextInt();
         // TODO: a, b, c 중 최대값 또는 같은 값 여부 출력
+
+        if (b < a && a > c) {
+            System.out.println(a + "첫 번째 숫자가 가장 큽니다. ");
+        } else if (a < b && b > c) {
+            System.out.println(b + "두 번째 숫자가 가장 큽니다. ");
+        } else if (b < c && c > a) {
+            System.out.println(c + "세 번째 숫자가 가장 큽니다. ");
+        } else if (a == b || b == c || c == a) {
+            System.out.println("같은 수가 존재합니다.");
+        } else {
+            System.out.println("숫자를 입력하세요 휴먼..");
+        }
     }
 }
