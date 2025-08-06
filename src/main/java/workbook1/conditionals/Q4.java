785dc5745a763cb40a887ac47100bbbea02b3886
grace-jsy <sseo.oee@gmail.com>
2025-08-06 17:25:15 +0900

    feat: [conditionals] Q4 solved

--- src/main/java/workbook1/conditionals/Q4.java
+++ src/main/java/workbook1/conditionals/Q4.java
@@ -7,5 +7,14 @@ public class Q4 {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
         // TODO: 윤년 판별 로직 구현
+
+        // 윤년은 연도가 4의 배수이면서 100의 배수가 아니거나 400의 배수인 해이다.
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " = 윤년");
+        } else {
+            System.out.println(year + " = 평년");
+        }
+
     }
 }
