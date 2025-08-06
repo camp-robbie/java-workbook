cdab3a0644ce294d006c2dd895454666bb3db33e
grace-jsy <sseo.oee@gmail.com>
2025-08-06 10:16:36 +0900

    feat: [primitiveconversion] Q5 add scanner and type casting

--- src/main/java/workbook1/primitiveconversion/Q5.java
+++ src/main/java/workbook1/primitiveconversion/Q5.java
@@ -4,10 +4,22 @@ import java.util.Scanner;
 
 public class Q5 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        // 스캐너로 입력 받기
+        System.out.print("x = ");
         double x = sc.nextDouble();
+        System.out.print("y = ");
         double y = sc.nextDouble();
+        System.out.print("z: ");
         double z = sc.nextDouble();
         // TODO: x, y, z를 int로 변환 후 평균을 float으로 출력
+
+        // int로 변환
+        int xx = (int) x;
+        int yy = (int) y;
+        int zz = (int) z;
+
+
     }
 }
