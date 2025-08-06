08397b549463879d2ead1f35faa490fb7902e1bc
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-06 20:49:59 +0900

    feat: [loops] Q2 구현

--- src/main/java/workbook1/loops/Q2.java
+++ src/main/java/workbook1/loops/Q2.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 
 public class Q2 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        // TODO: 홀수 합과 짝수 합 계산 후 출력
+
+        // 홀수의 합계와 짝수의 합계를 저장하는 변수를 선언합니다.
+        int oddSum = 0;
+        int evenSum = 0;
+
+        // for(인덱스 선언; 조건식; 증감식){코드}
+        // 홀수 <- i를 1로 선언하고, 2씩 더해줍니다.
+        for(int i=1;i<=n;i+=2){
+            oddSum += i;
+        }
+        // 짝수 <- i를 2로 선언하고, 2씩 더해줍니다.
+        for(int i=2;i<=n;i+=2){
+            evenSum += i;
+        }
+
+        // 출력합니다.
+        System.out.println(oddSum+" "+evenSum);
     }
 }
