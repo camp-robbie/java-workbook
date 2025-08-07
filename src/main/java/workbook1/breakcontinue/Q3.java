34d505b0c472e90f5ba5564958e43456eecd5bbd
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-07 22:27:18 +0900

    breakcontinue Q3 구현

--- src/main/java/workbook1/breakcontinue/Q3.java
+++ src/main/java/workbook1/breakcontinue/Q3.java
@@ -1,10 +1,29 @@
 package main.java.workbook1.breakcontinue;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // TODO: 음수가 나오면 break, 양수 평균 출력
+        try {
+            int sum = 0; // 양수들의 합을 담을 변수
+            int count = 0; // 반복문 횟수를 담을 변수
+
+            while (true) {
+                int n = sc.nextInt(); // 반복 입력
+                if (n < 0) { // 만약 입력 값이 음수라면
+                    break;
+                }
+                sum += n; // 양수일 때 sum = sum + n;
+                count++; // 반복문 실행 될 때 +1;
+            }
+
+            double positive = (double) sum / count; // (double로 형변환) 양수의 합 / 입력된 횟수
+            System.out.println("양수들의 평균 = " + positive);
+        } catch (InputMismatchException e) {
+            System.out.println("숫자만 입력해주세요!");
+        }
     }
 }
