0057d90dd9b1c79f4a0a2b990f826196c4681db8
cjn404 <moi404@naver.com>
2025-08-07 22:30:31 +0900

    feat: [loops] Q2 구현

--- src/main/java/workbook1/loops/Q2.java
+++ src/main/java/workbook1/loops/Q2.java
@@ -7,5 +7,20 @@ public class Q2 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: 홀수 합과 짝수 합 계산 후 출력
+
+        int sum1 = 0;   // 홀수
+        int sum2 = 0;   // 짝수
+
+        for (int i = 1; i <= n; i += 2) {
+            sum1 += i;
+        }
+        for (int i = 2; i <= n; i += 2) {
+            sum2 += i;
+        }
+
+        System.out.println(sum1 + " " + sum2);
+
+        sc.close();
+
     }
 }
