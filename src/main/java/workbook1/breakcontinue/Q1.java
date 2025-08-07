54e42ba7d2a562a318174c8609020e624124858b
t2023-m0121 <hardnokk@gmail.com>
2025-08-07 23:22:04 +0900

    [박신희] <break와 continue 활용> 제출

--- src/main/java/workbook1/breakcontinue/Q1.java
+++ src/main/java/workbook1/breakcontinue/Q1.java
@@ -2,8 +2,15 @@ package main.java.workbook1.breakcontinue;
 
 public class Q1 {
     public static void main(String[] args) {
-        int sum = 0;
         // TODO: 1부터 순차적으로 더하다가 10의 배수를 만나면 break
-        // 합을 출력
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 10 == 0) {
+                System.out.println(sum);
+                break;
+            } else {
+                sum += i;
+            }
+        }
     }
 }
