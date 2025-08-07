3a60fdfac375e6a16d0a04d2197eeada2f868b26
cjn404 <moi404@naver.com>
2025-08-07 23:17:31 +0900

    feat: [loops] Q4 구현

--- src/main/java/workbook1/loops/Q4.java
+++ src/main/java/workbook1/loops/Q4.java
@@ -8,5 +8,15 @@ public class Q4 {
         int m = sc.nextInt();
         int n = sc.nextInt();
         // TODO: 이중 for 문으로 별 패턴 출력
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+
     }
 }
