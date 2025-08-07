65a4675fb779488bb91d3dcf2736a927903dbb71
t2023-m0121 <hardnokk@gmail.com>
2025-08-07 16:27:47 +0900

    [박신희] <반복문과 중첩 반복문> 제출

--- src/main/java/workbook1/loops/Q5.java
+++ src/main/java/workbook1/loops/Q5.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class Q5 {
     public static void main(String[] args) {
+        // TODO: 역삼각형 패턴 출력
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();
-        // TODO: 역삼각형 패턴 출력
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < h - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
