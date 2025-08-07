f78e5bfcab547b9d0b07588311552c60d539f069
cjn404 <moi404@naver.com>
2025-08-07 23:37:54 +0900

    feat: [loops] Q6 구현

--- src/main/java/workbook1/loops/Q6.java
+++ src/main/java/workbook1/loops/Q6.java
@@ -3,5 +3,13 @@ package main.java.workbook1.loops;
 public class Q6 {
     public static void main(String[] args) {
         // TODO: 구구단 전체를 출력
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 2; j <= 9; j++) {
+                int mul = i * j;
+                System.out.print(j + "*" + i + "=" + mul + "\t");
+            }
+            System.out.println();
+        }
     }
 }
