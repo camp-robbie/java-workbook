e12fd8ece0986b16d2070eda7a7a22e008458db3
cjn404 <moi404@naver.com>
2025-08-08 00:53:07 +0900

    feat: [controlflow] Q1 구현

--- src/main/java/workbook1/controlflow/Q1.java
+++ src/main/java/workbook1/controlflow/Q1.java
@@ -5,7 +5,45 @@ import java.util.Scanner;
 public class Q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
         // TODO: 짝수는 합, 홀수는 곱에 반영 후 최종 결과 출력
+
+        int n;
+
+        // n = 0의 경우 잘못된 결과(0 1)이 출력
+        // 재입력문 출력
+        while (true) {
+            n = sc.nextInt();
+
+            if (n >= 1) {
+                break;
+            } else {
+                System.out.println("n은 1 이상의 정수여야 합니다.\n" + "재입력하시겠습니까? (No 입력 시 종료)");
+
+                sc.nextLine();
+                String no = sc.nextLine();
+
+                if ("No".equalsIgnoreCase(no)) {
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                } else {
+                    System.out.println("1 이상의 정수로 재입력 바랍니다.");
+                }
+            }
+        }
+
+        int sum = 0;
+        int mul = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            } else {
+                mul *= i;
+            }
+        }
+
+        System.out.println(sum + " " + mul);
+
+        sc.close();
     }
 }
