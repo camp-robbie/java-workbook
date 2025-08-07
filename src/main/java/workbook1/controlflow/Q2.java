870dde428771702d46b825d657784b38ddc498bf
t2023-m0121 <hardnokk@gmail.com>
2025-08-07 17:57:41 +0900

    [박신희] <반복문과 조건문 통합 활용> 제출

--- src/main/java/workbook1/controlflow/Q2.java
+++ src/main/java/workbook1/controlflow/Q2.java
@@ -1,11 +1,25 @@
 package main.java.workbook1.controlflow;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q2 {
     public static void main(String[] args) {
+        // TODO: count개의 정수를 입력받아 양수 개수와 음수 합을 계산 후 출력
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
-        // TODO: count개의 정수를 입력받아 양수 개수와 음수 합을 계산 후 출력
+        int pos = 0;
+        int neg = 0;
+
+        for(int i = 0; i < count; i++){ // count 수 만큼 정수 입력 받기
+            int num = sc.nextInt();
+            if(num > 0){
+                pos++;
+            }else{
+                neg += num;
+            }
+        }
+
+        System.out.println(pos + " " + neg);
     }
 }
--- src/main/java/workbook1/controlflow/Q5.java
+++ src/main/java/workbook1/controlflow/Q5.java
@@ -4,8 +4,20 @@ import java.util.Scanner;
 
 public class Q5 {
     public static void main(String[] args) {
+        // TODO: 각 자리수의 합을 반복해 한 자리 수가 될 때까지 계산 후 출력
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        // TODO: 각 자리수의 합을 반복해 한 자리 수가 될 때까지 계산 후 출력
+
+        while (num >= 10) {
+            int sum = 0;
+            String str = Integer.toString(num);  // 숫자를 문자열로 변환
+
+            for (int i = 0; i < str.length(); i++) {
+                sum += Integer.parseInt(str.substring(i, i + 1));
+                // 한 글자씩 떼서 숫자로 치환하여 더함
+            }
+            num = sum;
+        }
+        System.out.println(num);
     }
 }
