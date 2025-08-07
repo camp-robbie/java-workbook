43fc344e8ea9d885a439600c2712709cfd6c72ea
cjn404 <moi404@naver.com>
2025-08-08 01:06:27 +0900

    feat: [controlflow] Q2 구현

--- src/main/java/workbook1/controlflow/Q2.java
+++ src/main/java/workbook1/controlflow/Q2.java
@@ -7,5 +7,20 @@ public class Q2 {
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
         // TODO: count개의 정수를 입력받아 양수 개수와 음수 합을 계산 후 출력
+
+        int num1 = 0;   // 양수
+        int num2 = 0;   // 음수
+
+        for (int i = 0; i < count; i++) {
+            int num = sc.nextInt();
+            if (num > 0) {
+                num1++;
+            } else {
+                num2 += num;
+            }
+        }
+        System.out.println(num1 + " "  + num2);
+
+        sc.close();
     }
 }
