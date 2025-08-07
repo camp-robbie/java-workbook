ae26047d75c0cb87b7dc998d29b43e011b9555ef
cjn404 <moi404@naver.com>
2025-08-08 02:34:26 +0900

    feat: [controlflow] Q6 구현

--- src/main/java/workbook1/controlflow/Q6.java
+++ src/main/java/workbook1/controlflow/Q6.java
@@ -7,5 +7,26 @@ public class Q6 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: 1~n까지 중 자리수 합이 5의 배수인 수의 개수 출력
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int num = i;
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            // 5의 배수가 맞는지 검사
+            if (sum % 5 == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        sc.close();
+
     }
 }
