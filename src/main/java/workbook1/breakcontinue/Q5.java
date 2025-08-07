62b7aecd5612f9928b2079de63412c47150be361
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-07 22:54:20 +0900

    breakcontinue Q5 구현

--- src/main/java/workbook1/breakcontinue/Q5.java
+++ src/main/java/workbook1/breakcontinue/Q5.java
@@ -5,7 +5,12 @@ public class Q5 {
         outer:
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
-                // TODO: i + j == 6이면 위치 출력 후 라벨 break
+
+                // TODO: i + j == 6이면 위치 출력 후
+                if (i + j == 6) {
+                    System.out.println((i + "," + j)); // 위치 출력
+                    break outer; // 라벨 break;
+                }
             }
         }
     }
