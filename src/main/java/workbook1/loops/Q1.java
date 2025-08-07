68ab7c4d3ae2d7b02ae1ca481947e72f6fbcd946
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-07 11:17:40 +0900

    loops Q1 구현

--- src/main/java/workbook1/loops/Q1.java
+++ src/main/java/workbook1/loops/Q1.java
@@ -7,6 +7,16 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: while 문을 이용하여 1부터 n까지의 합 출력
+
+        int one = 1; // 조건이 1부터의 합이기 때문에 1을 변수에 저장
+        int sum = 0; // 결과 값을 구할 변수 생성
+        while (one <= n) { // 조건을 1부터 입력 값까지 반복하도록 설정
+            sum += one;
+            one++; // 후위 증가를 활용하여 +1을 증가 시킨 값을 one에 저장
+        }
+        System.out.println("결과 값은 : " + sum); // 결과 값 출력
+
+        sc.close(); // 자원 손실 방지
     }
 }
 
