aba3bba1e169124ee7d1b9b001d133fdc4fc22df
cjn404 <moi404@naver.com>
2025-08-08 09:02:40 +0900

    feat: [controlflow] Q1 예외 처리 추가

--- src/main/java/workbook1/controlflow/Q1.java
+++ src/main/java/workbook1/controlflow/Q1.java
@@ -1,5 +1,6 @@
 package main.java.workbook1.controlflow;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Q1 {
@@ -12,14 +13,28 @@ public class Q1 {
         // n = 0의 경우 잘못된 결과(0 1)이 출력
         // 재입력문 출력
         while (true) {
-            n = sc.nextInt();
+            try {
+                n = sc.nextInt();
 
-            if (n >= 1) {
-                break;
-            } else {
+                if (n >= 1) {
+                    break;
+                } else {
+                    System.out.println("n은 1 이상의 정수여야 합니다.\n" + "재입력하시겠습니까? (No 입력 시 종료)");
+
+                    sc.nextLine();
+                    String no = sc.nextLine();
+
+                    if ("No".equalsIgnoreCase(no)) {
+                        System.out.println("프로그램을 종료합니다.");
+                        return;
+                    } else {
+                        System.out.println("1 이상의 정수로 재입력 바랍니다.");
+                    }
+                }
+            } catch (InputMismatchException e) {
                 System.out.println("n은 1 이상의 정수여야 합니다.\n" + "재입력하시겠습니까? (No 입력 시 종료)");
 
-                sc.nextLine();
+                sc.nextLine();  // 버퍼 비우기 안 하면, 무한 루프
                 String no = sc.nextLine();
 
                 if ("No".equalsIgnoreCase(no)) {
