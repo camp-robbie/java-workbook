e1647d406eaca49400b266f28ee95a36e709ffa1
HY-WG <mung94c@gmail.com>
2025-08-08 15:00:42 +0900

    반복문과 중첩 반복문 풀이

--- src/main/java/workbook1/loops/Q1.java
+++ src/main/java/workbook1/loops/Q1.java
@@ -7,6 +7,17 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: while 문을 이용하여 1부터 n까지의 합 출력
+
+        int i = 1;
+        int sum = 0; // 합계를 저장하는 변수 설정
+
+        // i 가 n 초과가 될 때까지 반복
+        while (i <= n) {
+            sum += i; // sum = sum+i
+            i++;
+        }
+
+        System.out.println(sum);
     }
 }
 
--- src/main/java/workbook1/loops/Q2.java
+++ src/main/java/workbook1/loops/Q2.java
@@ -7,5 +7,25 @@ public class Q2 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: 홀수 합과 짝수 합 계산 후 출력
+
+        int oddSum = 0; // 홀수 합
+        int evenSum = 0; // 짝수 합
+
+        // 홀수 합 구하기: 1부터 시작, 2씩 증가
+        int i = 1;
+        while (i <= n) {
+            oddSum += i;
+            i += 2;
+        }
+        // 짝수 합 구하기 : 2부터 시작, 2씩 증가
+        int j = 2;
+        while (j <= n) {
+            evenSum += j;
+            j += 2;
+        }
+
+        System.out.println("홀수 합 = " + oddSum);
+        System.out.println("짝수 합 = " + evenSum);
+
     }
 }
