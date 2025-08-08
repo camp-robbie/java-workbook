c0d8c2c7b9883adb25cbed4f0781aac7b2c500c6
grace-jsy <sseo.oee@gmail.com>
2025-08-08 18:43:11 +0900

    feat: [controlflow] Q4 Unsolved

--- src/main/java/workbook1/controlflow/Q4.java
+++ src/main/java/workbook1/controlflow/Q4.java
@@ -6,5 +6,7 @@ public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // TODO: 0이 나올 때까지 양수만 합산하는 코드 작성
+
+        // 무슨 문제인지 조차 이해가 안됩니다ㅠ ㅠ
     }
 }
