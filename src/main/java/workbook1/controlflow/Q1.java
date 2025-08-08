86e7e91ea79e91a4b95f312e2feb82129399eb6d
cjn404 <moi404@naver.com>
2025-08-08 12:15:23 +0900

    feat: [controlflow] Q1 입출력 분리

--- src/main/java/workbook1/controlflow/Q1.java
+++ src/main/java/workbook1/controlflow/Q1.java
@@ -4,11 +4,30 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Q1 {
+
+    // 입출력 분리
+    public void InputOutput(Scanner sc) {
+
+        System.out.println("n은 1 이상의 정수여야 합니다.\n" + "재입력하시겠습니까? (No 입력 시 종료)");
+
+        sc.nextLine();
+        String no = sc.nextLine();
+
+        if ("No".equalsIgnoreCase(no)) {
+            System.out.println("프로그램을 종료합니다.");
+            return;
+        } else {
+            System.out.println("1 이상의 정수로 재입력 바랍니다.");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // TODO: 짝수는 합, 홀수는 곱에 반영 후 최종 결과 출력
 
         int n;
+
+        Q1 q = new Q1();
 
         // n = 0의 경우 잘못된 결과(0 1)이 출력
         // 재입력문 출력
@@ -19,30 +38,32 @@ public class Q1 {
                 if (n >= 1) {
                     break;
                 } else {
-                    System.out.println("n은 1 이상의 정수여야 합니다.\n" + "재입력하시겠습니까? (No 입력 시 종료)");
-
-                    sc.nextLine();
-                    String no = sc.nextLine();
-
-                    if ("No".equalsIgnoreCase(no)) {
-                        System.out.println("프로그램을 종료합니다.");
-                        return;
-                    } else {
-                        System.out.println("1 이상의 정수로 재입력 바랍니다.");
-                    }
+                    q.InputOutput(sc);
+//                    System.out.println("n은 1 이상의 정수여야 합니다.\n" + "재입력하시겠습니까? (No 입력 시 종료)");
+//
+//                    sc.nextLine();
+//                    String no = sc.nextLine();
+//
+//                    if ("No".equalsIgnoreCase(no)) {
+//                        System.out.println("프로그램을 종료합니다.");
+//                        return;
+//                    } else {
+//                        System.out.println("1 이상의 정수로 재입력 바랍니다.");
+//                    }
                 }
             } catch (InputMismatchException e) {
-                System.out.println("n은 1 이상의 정수여야 합니다.\n" + "재입력하시겠습니까? (No 입력 시 종료)");
-
-                sc.nextLine();  // 버퍼 비우기 안 하면, 무한 루프
-                String no = sc.nextLine();
-
-                if ("No".equalsIgnoreCase(no)) {
-                    System.out.println("프로그램을 종료합니다.");
-                    return;
-                } else {
-                    System.out.println("1 이상의 정수로 재입력 바랍니다.");
-                }
+                q.InputOutput(sc);
+//                System.out.println("n은 1 이상의 정수여야 합니다.\n" + "재입력하시겠습니까? (No 입력 시 종료)");
+//
+//                sc.nextLine();  // 버퍼 비우기 안 하면, 무한 루프
+//                String no = sc.nextLine();
+//
+//                if ("No".equalsIgnoreCase(no)) {
+//                    System.out.println("프로그램을 종료합니다.");
+//                    return;
+//                } else {
+//                    System.out.println("1 이상의 정수로 재입력 바랍니다.");
+//                }
             }
         }
 
