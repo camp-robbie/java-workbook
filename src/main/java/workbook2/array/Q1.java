4cb4dc5710e966d51eab3a3b709ab542840b912b
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-09 11:04:23 +0900

    array Q2 구현 Q1 코드 정리

--- src/main/java/workbook2/array/Q1.java
+++ src/main/java/workbook2/array/Q1.java
@@ -7,9 +7,9 @@ public class Q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[5]; // 크기가 5인 정수형 배열 선언
+        try {
+            int[] arr = new int[5]; // 크기가 5인 정수형 배열 선언
 
-        try{
             // TODO: arr에 5개 정수 저장 후 출력
             for (int i = 0; i < arr.length; i++) { // 배열의 길이 만큼 입력 받기
                 arr[i] = sc.nextInt();
@@ -17,13 +17,12 @@ public class Q1 {
 
             System.out.println("저장된 숫자 = ");
             for (int i = 0; i < arr.length; i++) { // 배열 안에 있는 숫자를 하나씩 출력
-                System.out.print(arr[i]+" "); // 출력 예시에 맞게 출력 조정
+                System.out.print(arr[i] + " "); // 출력 예시에 맞게 출력 조정
             }
-        } catch (InputMismatchException e){ // 숫자 이외의 입력을 할 시
+        } catch (InputMismatchException e) { // 숫자 이외의 입력을 할 시
             System.out.println("숫자만 입력해주세요!"); // 예외처리
-        } finally{ // 항상 실행
+        } finally { // 항상 실행
             sc.close(); // 자원 누수 방지
         }
-
     }
 }
--- src/main/java/workbook2/array/Q2.java
+++ src/main/java/workbook2/array/Q2.java
@@ -1,11 +1,41 @@
 package main.java.workbook2.array;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double[] arr = new double[10];
-        // TODO: arr에 값 저장 후 최대값과 최소값 출력
+
+        try {
+            double[] arr = new double[10];
+            // TODO: arr에 값 저장 후 최대값과 최소값 출력
+
+            arr[0] = sc.nextDouble();
+
+            // 입력 받기 전 초기 값은 double 이라 0.0
+            // max, min 초기 값 설정
+            double max = arr[0];
+            double min = arr[0];
+
+            for (int i = 1; i < arr.length; i++) { // 9번 반복
+                arr[i] = sc.nextDouble(); // 실수를 입력 고로 double
+
+                if (arr[i] > max) { // 가장 큰 수 찾는 로직
+                    max = arr[i]; // max에 값을 덮어쓴다.
+                }
+                if (arr[i] < min) { // 가장 작은 수 찾는 로직
+                    min = arr[i]; // min에 값을 덮어쓴다.
+                }
+            }
+
+            // 예시 출력과 비슷하게 출력
+            System.out.print("가장 큰 수는 = " + max + " ");
+            System.out.print("가장 작은 수는 = " + min);
+        } catch (InputMismatchException e) { // 숫자 이외의 입력을 할 시
+            System.out.println("숫자만 입력해주세요!"); // 예외처리
+        } finally {
+            sc.close(); // 자원 누수 방지
+        }
     }
 }
