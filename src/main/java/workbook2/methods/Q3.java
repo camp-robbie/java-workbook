d6a2bf6d575458a3b9a6be2248c8977a60b36e20
eunji-0508 <cojia@naver.com>
2025-08-10 18:02:59 +0900

    feat: [methods] Q3 구현 수정

--- src/main/java/workbook2/methods/Q3.java
+++ src/main/java/workbook2/methods/Q3.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Q3 {
     // TODO: addArrays 메서드 작성
+    // 메서드 이름은 addArrays, 매개변수는 두 개의 int[], 반환형은 int[]
+    public static int[] addArrays(int[] arr1, int[] arr2) {
+        int[] arr = new int[arr1.length];       // 크기를 arr1 길이로 설정
+        for(int i = 0; i < arr.length; i++) {   // 조건을 만족할 동안 반복 수행
+            arr[i] = arr1[i] + arr2[i];
+        }
+        return arr;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -15,6 +25,12 @@ public class Q3 {
         for (int i = 0; i < n; i++) {
             arr2[i] = sc.nextInt();
         }
+
         // TODO: addArrays 호출 후 결과 배열 출력
+        int[] resultArr = addArrays(arr1, arr2);    // addArrays 호출하여 resultArr에 저장함
+
+        for (int i : resultArr) {   // 결과 출력
+            System.out.print(i + " ");
+        }
     }
 }
