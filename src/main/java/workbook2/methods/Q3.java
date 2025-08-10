416173dcb73d97dcaf56f879437aa3309d87aa72
cjn404 <moi404@naver.com>
2025-08-11 01:38:06 +0900

    feat: [methods] Q3 구현

--- src/main/java/workbook2/methods/Q3.java
+++ src/main/java/workbook2/methods/Q3.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Q3 {
     // TODO: addArrays 메서드 작성
+    public int[] addArrays(int[] array1, int[] array2) {
+        int length = array1.length;
+        int[] result = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = array1[i] + array2[i];
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -16,5 +26,20 @@ public class Q3 {
             arr2[i] = sc.nextInt();
         }
         // TODO: addArrays 호출 후 결과 배열 출력
+        Q3 q3 = new Q3();
+
+        int[] sumArr = q3.addArrays(arr1, arr2);
+
+        for (int i = 0; i < sumArr.length; i++) {
+            System.out.print(sumArr[i]);
+            if (i != sumArr.length - 1) {
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println();
+
+        sc.close();
+
     }
 }
