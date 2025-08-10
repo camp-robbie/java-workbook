5fc98bdfa4bc939208eef687b4982d4c1c4a6ee8
cjn404 <moi404@naver.com>
2025-08-11 01:38:17 +0900

    feat: [methods] Q5 구현

--- src/main/java/workbook2/methods/Q5.java
+++ src/main/java/workbook2/methods/Q5.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Q5 {
     // TODO: reverseStrings 메서드 작성
+    public String[] reverseStrings(String[] arr) {
+        String[] result = new String[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = new StringBuilder(arr[i]).reverse().toString();
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -12,5 +21,23 @@ public class Q5 {
             words[i] = sc.next();
         }
         // TODO: reverseStrings 호출 후 결과 출력
+        Q5 q5 = new Q5();
+        String[] reversed = q5.reverseStrings(words);
+
+        for (int i = 0; i < reversed.length; i++) {
+            System.out.print(reversed[i]);
+            if (i != reversed.length - 1) {
+                System.out.print(" ");
+            }
+        }
+
+//        for (String str : reversed) {
+//            System.out.print(str + " ");
+//        }
+
+        System.out.println();
+
+        sc.close();
+
     }
 }
