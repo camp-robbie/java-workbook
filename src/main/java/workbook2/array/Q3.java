46649985f10ac09b96ac043486954eba267efc11
hailee2 <slowslow9150@gmail.com>
2025-08-10 13:56:06 +0900

    feat: [array] Q3 구현

--- src/main/java/workbook2/array/Q3.java
+++ src/main/java/workbook2/array/Q3.java
@@ -1,12 +1,24 @@
 package main.java.workbook2.array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //N을 입력받아 크기 N의 정수형 배열을 선언
         int n = sc.nextInt();
         int[] arr = new int[n];
-        // TODO: arr에 n개의 정수 저장 후 역순으로 출력
+        for(int j = 0; j < n; j++){
+            arr[j] = sc.nextInt();
+        }
+
+        // arr 역순으로 출력
+        int[] reversedArr = new int[n];
+        for(int i = 0; i < n; i++){
+            reversedArr[i] = arr[n-1-i];
+        }
+        String s = Arrays.toString(reversedArr);
+        System.out.println(s);
     }
 }
