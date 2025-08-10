bd0e635c843f9f7dec0a7d6bdb5da6ab7394e18c
cjn404 <moi404@naver.com>
2025-08-11 01:38:12 +0900

    feat: [methods] Q4 구현

--- src/main/java/workbook2/methods/Q4.java
+++ src/main/java/workbook2/methods/Q4.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 
 public class Q4 {
     // TODO: 세 정수의 평균을 반환하는 average 메서드 작성
+    public double average(int num1, int num2, int num3) {
+        return (double) (num1 + num2 + num3) / 3;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
         // TODO: average 호출 후 결과 출력
+
+        Q4 q4 = new Q4();
+
+        System.out.println(q4.average(a, b, c));
+
+        sc.close();
     }
 }
