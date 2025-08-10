52f50be10f3f254708aee05dc2da9470c1987309
eunji-0508 <cojia@naver.com>
2025-08-10 17:43:46 +0900

    feat: [methods] Q1 구현 수정

--- src/main/java/workbook2/methods/Q1.java
+++ src/main/java/workbook2/methods/Q1.java
@@ -1,13 +1,22 @@
 package main.java.workbook2.methods;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Q1 {
     // TODO: 두 정수를 더해 반환하는 sum 메서드를 작성하세요.
+    public static int sum(int a, int b){        // sum 메서드
+        return a + b;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         // TODO: sum 메서드를 호출하여 결과 출력
+        System.out.println(sum(a, b));  // sum 메서드 호출하여 출력
+
+        sc.close(); // 스캐너 닫기
     }
 }
