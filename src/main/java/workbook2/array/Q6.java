cbf43315b2a897bb1b83d29758fec381ba89f78f
cjn404 <moi404@naver.com>
2025-08-10 22:04:51 +0900

    feat: [array] Q6 구현

--- src/main/java/workbook2/array/Q6.java
+++ src/main/java/workbook2/array/Q6.java
@@ -9,5 +9,33 @@ public class Q6 {
         int cols = sc.nextInt();
         int[][] arr = new int[rows][cols];
         // TODO: 스네이크 패턴으로 arr 채우고 출력
+
+        int num = 1;    // 채울 숫자 초기화
+
+        // 스네이크 패턴으로 채우기
+        for (int i = 0; i < rows; i++) {
+            if (i % 2 == 0) {   // 짝수 행: 왼쪽 -> 오른쪽 ex) 0번째 행: 1 2 3 4
+                for (int j = 0; j < cols; j++) {
+                    arr[i][j] = num++;
+                }
+            } else {
+                for (int j = cols - 1; j >= 0; j--) {   // 홀수 행: 오른쪽 -> 왼쪽
+                    arr[i][j] = num++;
+                }
+            }
+        }
+
+        // 출력
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+
+        sc.close();
     }
 }
