005c684c6e28aef56e89b434d900b51063bf9ca3
leetwin1115 <jjeepp1115@gmail.com>
2025-08-11 14:37:45 +0900

    array-park-jungeun완료

--- src/main/java/workbook2/array/Q3.java
+++ src/main/java/workbook2/array/Q3.java
@@ -5,8 +5,23 @@ import java.util.Scanner;
 public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("정수형 배열 크기를 입력해주세요.");
         int n = sc.nextInt();
         int[] arr = new int[n];
         // TODO: arr에 n개의 정수 저장 후 역순으로 출력
+
+        System.out.println(n + "개의 정수를 입력해주세요.");
+        // arr에 n개의 정수 저장
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 배열을 역순으로 출력
+        System.out.println("배열을 거꾸로 출력합니다.");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
     }
 }
--- src/main/java/workbook2/array/Q6.java
+++ src/main/java/workbook2/array/Q6.java
@@ -9,5 +9,29 @@ public class Q6 {
         int cols = sc.nextInt();
         int[][] arr = new int[rows][cols];
         // TODO: 스네이크 패턴으로 arr 채우고 출력
+        int num = 1;
+
+        for (int i = 0; i < rows; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < cols; j++) {
+                    arr[i][j] = num++;
+                }
+            }
+            else {
+                for (int j = cols - 1; j >= 0; j--) {
+                    arr[i][j] = num++;
+                }
+            }
+        }
+
+        System.out.println("\n🐍스네이크 패턴 행렬:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("%4d", arr[i][j]);
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
