63dbf5c639345cd20a0a4875b1b70e19ea3b5436
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-11 20:42:40 +0900

    feat: [methods] Q3 구현

--- src/main/java/workbook2/methods/Q3.java
+++ src/main/java/workbook2/methods/Q3.java
@@ -9,12 +9,29 @@ public class Q3 {
         int n = sc.nextInt();
         int[] arr1 = new int[n];
         int[] arr2 = new int[n];
+
         for (int i = 0; i < n; i++) {
             arr1[i] = sc.nextInt();
         }
         for (int i = 0; i < n; i++) {
             arr2[i] = sc.nextInt();
         }
-        // TODO: addArrays 호출 후 결과 배열 출력
+
+        // 메서드를 호출해 return된 값을 새로운 배열에 담습니다.
+        int[] resultArr = addArrays(arr1, arr2);
+
+        // 출력합니다.
+        for (int i :  resultArr) {
+            System.out.print(i+" ");
+        }
+    }
+    static int[] addArrays(int[] arr1, int[] arr2) {
+        // 배열을 return 하는 구문이기 때문에, 새로운 배열을 선언합니다.
+        int[] sumArr = new int[arr1.length];
+        // arr1과 arr2의 요소를 더해 배열에 할당하는 구문입니다.
+        for (int i = 0; i < arr1.length; i++) {
+            sumArr[i] = arr1[i] + arr2[i];
+        }
+        return sumArr;
     }
 }
