c9597582c16267f9f924eb83b6d4dbe2cd48ea0a
hailee2 <slowslow9150@gmail.com>
2025-08-11 10:52:10 +0900

    feat: [paramtypes] Q6 구현

--- src/main/java/workbook2/paramtypes/Q6.java
+++ src/main/java/workbook2/paramtypes/Q6.java
@@ -4,12 +4,28 @@ import java.util.Scanner;
 
 public class Q6 {
     // TODO: addExclamation(String s)와 addExclamation(StringBuilder sb) 메서드 작성
+    private static String addExclamation(String s){
+        return s+"!";
+    }
+
+    private static StringBuilder addExclamation(StringBuilder sb){
+        return sb.append("!");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         StringBuilder sb = new StringBuilder(sc.next());
-        // addExclamation(s); // TODO: addExclamation(String s) 메서드 작성 후 주석 해제
-        // addExclamation(sb); // TODO: addExclamation(StringBuilder sb) 메서드 작성 후 주석 해제
+
+        addExclamation(s);
+        addExclamation(sb);
+
         // TODO: s와 sb 출력
+        System.out.println("불변 객체 String: " + s);
+        System.out.println("가변 객체 StringBuilder: " + sb);
+
+        // 리턴값 저장
+        s = addExclamation(s);
+        System.out.println("불변 객체 String (리턴값 저장): " + s);
     }
 }
