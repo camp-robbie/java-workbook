54a2927ce23a3ae0d30463f1a4e6bb629a937f73
grace-jsy <sseo.oee@gmail.com>
2025-08-11 16:54:37 +0900

    feat: [primitiveconversion] Q5 solved

--- src/main/java/workbook1/primitiveconversion/Q5.java
+++ src/main/java/workbook1/primitiveconversion/Q5.java
@@ -4,10 +4,26 @@ import java.util.Scanner;
 
 public class Q5 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double x = sc.nextDouble();
-        double y = sc.nextDouble();
-        double z = sc.nextDouble();
         // TODO: x, y, z를 int로 변환 후 평균을 float으로 출력
+
+        Scanner sc = new Scanner(System.in);
+
+        // x, y, z를 입력 받는다.
+        System.out.print("x: ");
+        double x = sc.nextDouble();
+        System.out.print("y: ");
+        double y = sc.nextDouble();
+        System.out.print("z: ");
+        double z = sc.nextDouble();
+
+        // 입력받은 숫자를 int로 변환한다.
+        int xx = (int) x;
+        int yy = (int) y;
+        int zz = (int) z;
+
+        // 평균을 float으로 출력한다.
+        float avg = (xx + yy + zz) / 3f;
+        System.out.println("float avg = " + avg);
+
     }
 }
