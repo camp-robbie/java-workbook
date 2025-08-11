d48861ef18695365aa180c45cc232c3e18987027
grace-jsy <sseo.oee@gmail.com>
2025-08-11 16:17:51 +0900

    feat: [primitiveconversion] Q1 solved

--- src/main/java/workbook1/primitiveconversion/Q1.java
+++ src/main/java/workbook1/primitiveconversion/Q1.java
@@ -4,9 +4,23 @@ import java.util.Scanner;
 
 public class Q1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
         // TODO: a와 b를 double로 자동 형변환하여 더한 뒤 int로 강제 변환하여 출력
+
+        Scanner sc = new Scanner(System.in);
+
+        // a와 b를 입력 받는다.
+        System.out.print("a: ");
+        int a = sc.nextInt();
+        System.out.print("b: ");
+        int b = sc.nextInt();
+
+        // 입력받은 숫자를 double로 자동 형변환한 뒤 더한다.
+        double sum = (double) a + b;
+        System.out.println("double sum: " + sum);
+
+        // int로 강제 변환하여 출력
+        int intSum = (int) sum;
+        System.out.println("int sum = " + intSum);
+
     }
 }
