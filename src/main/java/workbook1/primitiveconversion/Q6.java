3471d43e495b06cb3d08947ba2b25fddd36cb60d
grace-jsy <sseo.oee@gmail.com>
2025-08-11 17:05:35 +0900

    feat: [primitiveconversion] Q6 solved

--- src/main/java/workbook1/primitiveconversion/Q6.java
+++ src/main/java/workbook1/primitiveconversion/Q6.java
@@ -4,9 +4,23 @@ import java.util.Scanner;
 
 public class Q6 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s1 = sc.next();
-        String s2 = sc.next();
         // TODO: s1, s2를 int로 변환 후 차이를 byte로 변환하여 출력
+
+        Scanner sc = new Scanner(System.in);
+
+        // s1, s2를 입력 받는다.
+        System.out.print("s1: ");
+        String s1 = sc.next();
+        System.out.print("s2: ");
+        String s2 = sc.next();
+
+        // s1, s2를 int로 변환
+        int ss11 = Integer.parseInt(s1);
+        int ss22 = Integer.parseInt(s2);
+
+        // 차이를 byte로 변환하여 출력
+        byte difference = (byte) (ss11 - ss22);
+        System.out.println("difference = " + difference);
+
     }
 }
