d7d1a886a7e36842bbbae60d42ae816083241020
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-13 01:57:24 +0900

    feat: [varcompare] Q2 구현

--- src/main/java/workbook2/varcompare/Q2.java
+++ src/main/java/workbook2/varcompare/Q2.java
@@ -3,25 +3,40 @@ package main.java.workbook2.varcompare;
 import java.util.Scanner;
 
 class Car {
+    // 필드
     static int totalCars = 0;
-    String name;
+    private final String name;
+
+    // getter
+    public String getName(){
+        return name;
+    }
+
+    // 생성자
     Car(String name) {
         this.name = name;
-        // TODO: totalCars 증가
+        Car.totalCars++;
     }
 }
 
 public class Q2 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        // 차 이름 입력
         String n1 = sc.next();
         String n2 = sc.next();
         String n3 = sc.next();
+
+        // 객체 생성
         Car c1 = new Car(n1);
         Car c2 = new Car(n2);
         Car c3 = new Car(n3);
-        System.out.println(c1.name + " " + Car.totalCars);
-        System.out.println(c2.name + " " + Car.totalCars);
-        System.out.println(c3.name + " " + Car.totalCars);
+
+        // 출력
+        System.out.println(c1.getName() + " " + Car.totalCars);
+        System.out.println(c2.getName() + " " + Car.totalCars);
+        System.out.println(c3.getName() + " " + Car.totalCars);
     }
 }
