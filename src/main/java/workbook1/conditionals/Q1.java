3a46170848f59edc5ddbd02bf25e43c26636cea3
grace-jsy <sseo.oee@gmail.com>
2025-08-12 09:33:04 +0900

    feat: [conditionals] Q1 solved

--- src/main/java/workbook1/conditionals/Q1.java
+++ src/main/java/workbook1/conditionals/Q1.java
@@ -4,8 +4,21 @@ import java.util.Scanner;
 
 public class Q1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
         // TODO: num의 부호를 판단하여 출력
+
+        Scanner sc = new Scanner(System.in);
+
+        // 사용자에게 정수를 입력 받는다.
+        System.out.print("Number: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("Positive Number");
+        } else if (num < 0) {
+            System.out.println("Negative Number");
+        } else {
+            System.out.println("0");
+        }
+
     }
 }
