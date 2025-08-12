7ffe3b08d465de48e848c870975166e314f9e393
eunji-0508 <cojia@naver.com>
2025-08-13 01:52:35 +0900

    feat: [varscope] Q2 구현

--- src/main/java/workbook2/varscope/Q2.java
+++ src/main/java/workbook2/varscope/Q2.java
@@ -8,5 +8,7 @@ public class Q2 {
     public static void main(String[] args) {
         showLocal();
         // TODO: msg 변수에 접근하려고 하면 컴파일 오류
+        // 지역 변수이기 때문에 외부에서 호출하면 컴파일 오류가 뜸
+//        System.out.println(msg);
     }
 }
