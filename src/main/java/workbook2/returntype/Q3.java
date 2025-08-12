f174f4826a221059ef4d9b9828d925b5e15cdb62
hailee2 <slowslow9150@gmail.com>
2025-08-12 16:51:00 +0900

    feat: [returntype] Q3 구현

--- src/main/java/workbook2/returntype/Q3.java
+++ src/main/java/workbook2/returntype/Q3.java
@@ -4,16 +4,36 @@ import java.util.Scanner;
 
 public class Q3 {
     // TODO: copyArray 메서드 작성 (int[] 배열을 받아 깊은 복사 후 반환)
+    public static int[] copyArray(int[] original){
+        int[] copy = new int[original.length];
+        for(int i = 0; i<original.length; i++){
+            copy[i] =  original[i];
+        }
+        return copy;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("배열 크기 입력");
         int n = sc.nextInt();
         int[] original = new int[n];
-        for (int i = 0; i < n; i++) original[i] = sc.nextInt();
-        // int[] copy = copyArray(original); // TODO: copyArray 메서드 작성 후 주석 해제
+
+        System.out.println("원본 배열 값 입력");
+        for (int i = 0; i < n; i++) {
+            original[i] = sc.nextInt();
+        }
+
+        int[] copy = copyArray(original);
+
         // 원본 배열 변경
+        System.out.println("배열값 변경");
         for (int i = 0; i < n; i++) {
             original[i] = sc.nextInt();
         }
         // TODO: copy 배열을 출력
+        System.out.println("복사된 배열값 출력");
+        for (int i : copy) {
+            System.out.print( i + " ");
+        }
     }
 }
