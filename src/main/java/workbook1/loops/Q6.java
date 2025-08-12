f575a768939e5d580f4aa825b7aef4e6897bc007
grace-jsy <sseo.oee@gmail.com>
2025-08-12 22:09:57 +0900

    feat: [loops] Q6 solved

--- src/main/java/workbook1/loops/Q6.java
+++ src/main/java/workbook1/loops/Q6.java
@@ -3,5 +3,12 @@ package main.java.workbook1.loops;
 public class Q6 {
     public static void main(String[] args) {
         // TODO: 구구단 전체를 출력
+
+        for (int dan = 2; dan <= 9; dan++) {
+            for (int i = 1; i <= 9; i++) {
+                System.out.print(dan + " * " + i + " = " + (dan * i) + "\t");
+            }
+            System.out.println();
+        }
     }
 }
