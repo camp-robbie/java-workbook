4912dbf1645ddf5d60bddc718fb0ec0c7c3baee1
eunji-0508 <cojia@naver.com>
2025-08-13 01:00:55 +0900

    feat: [classdesign] Q3 구현

--- src/main/java/workbook2/classdesign/Q3.java
+++ src/main/java/workbook2/classdesign/Q3.java
@@ -7,8 +7,16 @@ class Book {
     String author;
     int price;
     // TODO: 생성자 작성
+    // 생성자 (매개변수: String title, String author, int price)
+    public Book(String title, String author, int price) {
+        this.title = title;         // 매개변수 title 값을 Book 필드 title에 대입함
+        this.author = author;       // 매개변수 author 값을 Book 필드 author에 대입함
+        this.price = price;         // 매개변수 price 값을 Book 필드 price에 대입함
+    }
     void printInfo() {
         // TODO: 정보 출력
+        // Title: JavaProgramming, Author: John, Price: 20000 형태로 출력함
+        System.out.println("Title: " + title + ", Author: " + author + ", Price: " + price);
     }
 }
 
@@ -21,9 +29,9 @@ public class Q3 {
         String t2 = sc.next();
         String a2 = sc.next();
         int p2 = sc.nextInt();
-        // Book b1 = new Book(t1, a1, p1); // TODO: 생성자 작성 후 주석 해제
-        // Book b2 = new Book(t2, a2, p2); // TODO: 생성자 작성 후 주석 해제
-        // b1.printInfo(); // TODO: 생성자 작성 후 주석 해제
-        // b2.printInfo(); // TODO: 생성자 작성 후 주석 해제
+        Book b1 = new Book(t1, a1, p1); // TODO: 생성자 작성 후 주석 해제
+         Book b2 = new Book(t2, a2, p2); // TODO: 생성자 작성 후 주석 해제
+         b1.printInfo(); // TODO: 생성자 작성 후 주석 해제
+         b2.printInfo(); // TODO: 생성자 작성 후 주석 해제
     }
 }
