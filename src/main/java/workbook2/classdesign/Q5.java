c9f12e9f3b2243f47b5f0387a2e8cbd5dea0f2b1
hailee2 <slowslow9150@gmail.com>
2025-08-12 14:44:55 +0900

    feat: [classdesign] Q5 구현

--- src/main/java/workbook2/classdesign/Q5.java
+++ src/main/java/workbook2/classdesign/Q5.java
@@ -5,28 +5,53 @@ import java.util.Scanner;
 class Account {
     String accountNo;
     int balance;
+
     // TODO: 생성자, deposit, withdraw 메서드 작성
+    public Account(String accountNo, int balance) {
+        this.accountNo = accountNo;
+        this.balance = balance;
+    }
+
+    public void deposit(int amt){
+        balance += amt;
+        System.out.println("입금 후 금액 : " + balance);
+    }
+
+    public void withdraw(int amt){
+        balance -= amt;
+        if(balance>=0){
+            System.out.println("출금 후 잔액: " + balance);
+        }else {
+            System.out.println("잔액부족");
+        }
+    }
 }
 
 public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String accNo = sc.next();
         int bal = sc.nextInt();
-        // Account acc = new Account(accNo, bal); // TODO: 생성자 작성 후 주석 해제
+
+        Account acc = new Account(accNo, bal); // TODO: 생성자 작성 후 주석 해제
+
         String cmd1 = sc.next();
         int amt1 = sc.nextInt();
+
         if (cmd1.equals("deposit")) {
-            // acc.deposit(amt1); // TODO: deposit 메서드 작성 후 주석 해제
+            acc.deposit(amt1); // TODO: deposit 메서드 작성 후 주석 해제
         } else {
-            // acc.withdraw(amt1); // TODO: withdraw 메서드 작성 후 주석 해제
+            acc.withdraw(amt1); // TODO: withdraw 메서드 작성 후 주석 해제
         }
         String cmd2 = sc.next();
         int amt2 = sc.nextInt();
         if (cmd2.equals("deposit")) {
-            // acc.deposit(amt2); // TODO: deposit 메서드 작성 후 주석 해제
+            acc.deposit(amt2); // TODO: deposit 메서드 작성 후 주석 해제
         } else {
-            // acc.withdraw(amt2); // TODO: withdraw 메서드 작성 후 주석 해제
+            acc.withdraw(amt2); // TODO: withdraw 메서드 작성 후 주석 해제
         }
+
+        sc.close();
     }
 }
