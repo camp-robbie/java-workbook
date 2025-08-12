8f7ff1c2e22c8e06afa9e8ec5a68ee2b7cc7b9df
hailee2 <slowslow9150@gmail.com>
2025-08-12 21:54:13 +0900

    feat: [varscope] Q2

--- src/main/java/workbook2/varscope/Q2.java
+++ src/main/java/workbook2/varscope/Q2.java
@@ -8,5 +8,7 @@ public class Q2 {
     public static void main(String[] args) {
         showLocal();
         // TODO: msg 변수에 접근하려고 하면 컴파일 오류
+        // System.out.println(msg); msg를 읽어들일 수 없음 > 변수 msg는 매서드 안에서만 작동하기 때문에!
+
     }
 }
