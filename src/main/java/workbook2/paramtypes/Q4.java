c044ca0304ed3f7969954b345e349c594f480769
leetwin1115 <jjeepp1115@gmail.com>
2025-08-13 17:16:03 +0900

    paramtypes-park-jungeun완료

--- src/main/java/workbook2/paramtypes/Q4.java
+++ src/main/java/workbook2/paramtypes/Q4.java
@@ -8,13 +8,17 @@ class Person {
 }
 
 public class Q4 {
+    public static void changeName(Person p, String newName) {
+        p.name = newName;
+    }
     // TODO: changeName 메서드 작성 (Person 객체와 새 이름을 받아 객체의 name 수정)
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String oldName = sc.next();
         String newName = sc.next();
         Person p = new Person(oldName);
-        // changeName(p, newName); // TODO: changeName 메서드 작성 후 주석 해제
+        changeName(p, newName); // TODO: changeName 메서드 작성 후 주석 해제
         // TODO: p.name 출력
+        System.out.println(p.name);
     }
 }
--- src/main/java/workbook2/paramtypes/Q6.java
+++ src/main/java/workbook2/paramtypes/Q6.java
@@ -3,13 +3,20 @@ package main.java.workbook2.paramtypes;
 import java.util.Scanner;
 
 public class Q6 {
+    public static void addExclamation(String s) {
+        s += "!";
+    }
+    public static void addExclamation(StringBuilder sb) {
+        sb.append("!");
+    }
     // TODO: addExclamation(String s)와 addExclamation(StringBuilder sb) 메서드 작성
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         StringBuilder sb = new StringBuilder(sc.next());
-        // addExclamation(s); // TODO: addExclamation(String s) 메서드 작성 후 주석 해제
-        // addExclamation(sb); // TODO: addExclamation(StringBuilder sb) 메서드 작성 후 주석 해제
+        addExclamation(s); // TODO: addExclamation(String s) 메서드 작성 후 주석 해제
+        addExclamation(sb); // TODO: addExclamation(StringBuilder sb) 메서드 작성 후 주석 해제
         // TODO: s와 sb 출력
+        System.out.println(s + " " + sb);
     }
 }
