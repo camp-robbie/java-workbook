a9b7ed8a51b7355c506e9c45a60aa010dac23fc0
HY-WG <mung94c@gmail.com>
2025-08-14 15:54:55 +0900

    array 풀이

--- src/main/java/workbook2/array/Q2.java
+++ src/main/java/workbook2/array/Q2.java
@@ -7,5 +7,17 @@ public class Q2 {
         Scanner sc = new Scanner(System.in);
         double[] arr = new double[10];
         // TODO: arr에 값 저장 후 최대값과 최소값 출력
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextDouble();
+        }
+
+        double max = arr[0];
+        double min = arr[0];
+
+        for(int i = 0; i < 10; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+        System.out.println(max + " " +min);
     }
 }
