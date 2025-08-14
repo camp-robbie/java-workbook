9fa29698f205630ca65594a5d9354181e8d72472
Xellos216 <xellos216@gmail.com>
2025-08-14 22:49:09 +0900

    [임홍민] 배열

--- src/main/java/workbook2/array/Q2.java
+++ src/main/java/workbook2/array/Q2.java
@@ -6,6 +6,16 @@ public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double[] arr = new double[10];
-        // TODO: arr에 값 저장 후 최대값과 최소값 출력
+
+        for (int i = 0; i < arr.length; i++) arr[i] = sc.nextDouble();
+
+        double max = arr[0];
+        double min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+        System.out.println(max + " " + min);
     }
 }
--- src/main/java/workbook2/array/Q5.java
+++ src/main/java/workbook2/array/Q5.java
@@ -7,6 +7,22 @@ public class Q5 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        // TODO: arr에 값 저장 후 버블 정렬 구현, 정렬된 배열 출력
+
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j  + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
     }
 }
+
+// 1. t에 왼쪽 값(arr[j]) 임시 저장
+// 2. 왼쪽(arr[j])에 오른쪽 값(arr[j+1]) 복사 → 두 값이 같아짐
+// 3. 오른쪽(arr[j+1])에 t(원래 왼쪽 값) 대입 → 자리 교환한 것이 됌
