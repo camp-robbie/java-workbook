b4e588067f3eaa0040d264ef5976f243a32eba86
hailee2 <slowslow9150@gmail.com>
2025-08-15 19:09:30 +0900

    feat: [varinit] Q3

--- src/main/java/workbook3/varinit/Q3.java
+++ src/main/java/workbook3/varinit/Q3.java
@@ -1,20 +1,20 @@
 package main.java.workbook3.varinit;
 
 public class Q3 {
-    static int s;
-    int i;
-    static {
+    static int s;                                       //Static 변수
+    int i;                                              //
+    static {                                            //가장 먼저 출력
         System.out.println("static block");
     }
     {
-        System.out.println("instance block");
+        System.out.println("instance block");           //세 번째 출력
     }
     Q3() {
         System.out.println("constructor");
-    }
+    }           //네 번째 출력
     public static void main(String[] args) {
-        System.out.println("static variable");
+        System.out.println("static variable");          //두 번째 출력
         Q3 obj = new Q3();
-        System.out.println("instance variable");
+        System.out.println("instance variable");        //다섯 번째 출력
     }
 }
