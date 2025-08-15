8bfb4627767afd1126b3d32dc8f67ba797b9b538
ehy0419 <2hy0419@naver.com>
2025-08-15 18:15:19 +0900

    feat: [classdesign] Q1 구현

--- src/main/java/workbook2/classdesign/Q1.java
+++ src/main/java/workbook2/classdesign/Q1.java
@@ -2,22 +2,49 @@ package main.java.workbook2.classdesign;
 
 import java.util.Scanner;
 
+/**
+ * 직사각형을 표현하는 간단한 클래스입니다.
+ * - 필드: 가로(width), 세로(height)
+ * - 메서드: getArea() -> width * height 반환
+ */
+
 class Rectangle {
-    int width;
-    int height;
+    int width;      // 가로
+    int height;     // 세로
     // TODO: 생성자 작성
+
+    /**
+     * 생성자: 가로와 세로를 받아 필드를 초기화합니다.
+     */
+
+    Rectangle(int width, int height) {
+        this.width = width;             // this.width (필드) = width; 매개변수 (지역 변수)
+        this.height = height;
+    }
+
+    /**
+     * 넓이 = 가로 * 세로
+     */
+
     int getArea() {
         // TODO: 넓이 반환
         return 0;
     }
 }
 
-public class Q1 {
+public class Q1 {       // Rectangle (넓이) 구하기
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int w = sc.nextInt();
         int h = sc.nextInt();
-        // Rectangle rect = new Rectangle(w, h); // TODO: 생성자 작성 후 주석 해제
+
+        Rectangle rect = new Rectangle(w, h); // TODO: 생성자 작성 후 주석 해제
         // TODO: rect.getArea() 출력
+
+        // 넓이 출력
+        System.out.println(rect.getArea());
+
+        sc.close();
     }
 }
