ceb7d164f7f948ea3bdfc64ad9440ef6caba9eab
hailee2 <slowslow9150@gmail.com>
2025-08-15 15:24:46 +0900

    feat: [methodcompare] Q3 구현

--- src/main/java/workbook2/methodcompare/Q3.java
+++ src/main/java/workbook2/methodcompare/Q3.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 class Converter {
     static double toCelsius(double f) {
         // TODO: 섭씨로 변환
-        return 0;
+        return (f - 32) * 5/9;
     }
     double toFahrenheit(double c) {
         // TODO: 화씨로 변환
-        return 0;
+        return c * 9/5 + 32;
     }
 }
 
@@ -18,9 +18,12 @@ public class Q3 {
         Scanner sc = new Scanner(System.in);
         double f = sc.nextDouble();
         double c = sc.nextDouble();
-        double cRes = Converter.toCelsius(f);
-        Converter conv = new Converter();
-        double fRes = conv.toFahrenheit(c);
+        double cRes = Converter.toCelsius(f);   //클래스(Static)메서드 호출
+        Converter conv = new Converter();       //객체생성
+        double fRes = conv.toFahrenheit(c);     //객체생성을 통한 인스턴스 메서드 호출
         // TODO: cRes와 fRes 출력
+        System.out.println("섭씨변환 : "+cRes + " | 화씨변환 : " + fRes);
+
+        sc.close();
     }
 }
