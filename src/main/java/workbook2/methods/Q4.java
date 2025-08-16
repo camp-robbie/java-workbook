7bfdf7f59763eea11dbc417647df547030a34abf
Xellos216 <xellos216@gmail.com>
2025-08-16 21:00:45 +0900

    Solve methods

--- src/main/java/workbook2/methods/Q4.java
+++ src/main/java/workbook2/methods/Q4.java
@@ -3,12 +3,16 @@ package main.java.workbook2.methods;
 import java.util.Scanner;
 
 public class Q4 {
-    // TODO: 세 정수의 평균을 반환하는 average 메서드 작성
+    public static double average(int a, int b, int c) {
+        return (a + b + c) / 3.0;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        // TODO: average 호출 후 결과 출력
+
+        System.out.print(average(a, b, c));
     }
 }
--- src/main/java/workbook2/methods/Q6.java
+++ src/main/java/workbook2/methods/Q6.java
@@ -3,10 +3,14 @@ package main.java.workbook2.methods;
 import java.util.Scanner;
 
 public class Q6 {
-    // TODO: 재귀를 이용한 factorial 메서드 작성
+    public static int factorial(int n) {
+        if(n==0) return 1;
+        return n * factorial(n-1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        // TODO: factorial 호출 후 결과 출력
+        System.out.print(factorial(n));
     }
 }
