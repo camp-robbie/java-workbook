1e3dd78201416005ff9af47aa74cb19f39fda3b6
Xellos216 <xellos216@gmail.com>
2025-08-16 21:39:48 +0900

    Solve paramtypes

--- src/main/java/workbook2/paramtypes/Q2.java
+++ src/main/java/workbook2/paramtypes/Q2.java
@@ -3,15 +3,20 @@ package main.java.workbook2.paramtypes;
 import java.util.Scanner;
 
 public class Q2 {
-    // TODO: addHundred 메서드 작성 (int[] arr의 첫 요소에 100을 더함)
+    public static void addHundred(int[] arr) {
+        if (arr.length > 0) arr[0] += 100;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        addHundred(arr);
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            if (i > 0) System.out.print(" ");
+            System.out.print(arr[i] + " ");
         }
-        // addHundred(arr); // TODO: addHundred 메서드 작성 후 주석 해제
-        // TODO: 변경된 arr 출력
     }
 }
