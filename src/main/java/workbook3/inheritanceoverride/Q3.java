786f86e0e800bef48b1aa39708395edfa35867e8
hailee2 <slowslow9150@gmail.com>
2025-08-16 15:18:07 +0900

    feat: [inheritanceoverride] Q3 주석추가

--- src/main/java/workbook3/inheritanceoverride/Q3.java
+++ src/main/java/workbook3/inheritanceoverride/Q3.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 class Vehicle {
     void move() {
         System.out.println("Vehicle is moving");
-    }
+    }           //부모클래스 메서드
 }
 
 class Car extends Vehicle {
     void move() {
         // TODO: "Car is moving" 출력
+        System.out.println("Car is moving");
     }
 }
 
 class Bike extends Vehicle {
     void move() {
         // TODO: "Bike is moving" 출력
+        System.out.println("Bike is moving");
     }
 }
 
@@ -26,9 +28,9 @@ public class Q3 {
         Vehicle[] vehicles = new Vehicle[3];
         for (int i = 0; i < 3; i++) {
             String type = sc.next();
-            vehicles[i] = type.equals("car") ? new Car() : new Bike();
+            vehicles[i] = type.equals("car") ? new Car() : new Bike();      //입력값 "car"이면 Car객체 생성, 아니면 Bike객체 생성
         }
-        for (Vehicle v : vehicles) {
+        for (Vehicle v : vehicles) {                                        //배열 vehicles 값 출력
             v.move();
         }
     }
