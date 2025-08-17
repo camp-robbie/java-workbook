cb90a1644421fd24cc228e0a62e491cbb579de70
grace-jsy <sseo.oee@gmail.com>
2025-08-17 20:03:01 +0900

    feat: [controlflow] Q2 solved

--- src/main/java/workbook1/controlflow/Q2.java
+++ src/main/java/workbook1/controlflow/Q2.java
@@ -4,8 +4,28 @@ import java.util.Scanner;
 
 public class Q2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
         // TODO: count개의 정수를 입력받아 양수 개수와 음수 합을 계산 후 출력
+
+        // 스캐너를 통해 숫자를 입력 받는다
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Counts : ");
+        int count = sc.nextInt();
+
+        int positiveCounts = 0;
+        int negativeSum = 0;
+
+       for (int i = 0; i < count; i++) {
+           int numbers = sc.nextInt();
+
+           if (numbers > 0) {
+               positiveCounts++;
+           } else if (numbers < 0) {
+               negativeSum += numbers;
+           }
+       }
+        System.out.println(positiveCounts + " " + negativeSum);
+
+       // 갯수를 세는 코드를 몰라 답지를 보고 적었습니다 (positiveCounts++)
+
     }
 }
