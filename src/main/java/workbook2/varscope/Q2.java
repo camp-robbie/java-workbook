aff5a35f262b07e3f368ceee843ab164ac9bfcb3
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-17 12:17:16 +0900

    varscope Q2 구현

--- src/main/java/workbook2/varscope/Q2.java
+++ src/main/java/workbook2/varscope/Q2.java
@@ -8,5 +8,9 @@ public class Q2 {
     public static void main(String[] args) {
         showLocal();
         // TODO: msg 변수에 접근하려고 하면 컴파일 오류
+        // System.out.println(msg);
+        // 같은 클래스에 있더라도 메서드가 다르다면 사용하지 못한다...
+        // 컴파일 오류 발생
+        // java: cannot find symbol
     }
 }
