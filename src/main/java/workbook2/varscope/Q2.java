4c56f51cacb27949c6e4f64a3e9011cc3c2f8a5d
jinhee kim <lsa68803@naver.com>
2025-08-17 19:00:30 +0900

    feat: [varscope] Q2 구현

--- src/main/java/workbook2/varscope/Q2.java
+++ src/main/java/workbook2/varscope/Q2.java
@@ -1,12 +1,17 @@
 package main.java.workbook2.varscope;
 
 public class Q2 {
-    static void showLocal() {
-        String msg = "inside";
-        System.out.println(msg);
+    static void showLocal() {            // showLocal() 메서드
+        String msg = "inside";           // 지역변수 msg에 "inside" 값 저장
+        System.out.println(msg);         // msg 출력
     }
     public static void main(String[] args) {
-        showLocal();
+        showLocal();                                            // 메서드 호출은 가능
         // TODO: msg 변수에 접근하려고 하면 컴파일 오류
+        /* System.out.println(msg);  -> 지역변수에 직접 접근 시도
+           위 출력 실행 시, 컴파일 오류 발생!
+           msg는 showLocal() 메서드에서만 사용가능한 지역변수 이므로,
+           메서드 외부에서 호출 시 컴파일 오류 발생.
+         */
     }
 }
