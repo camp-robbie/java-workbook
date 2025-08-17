95004b97730469936fc68ead5f9b9f2fc83bd57c
Xellos216 <xellos216@gmail.com>
2025-08-17 21:41:03 +0900

    Solve methodcompare

--- src/main/java/workbook2/methodcompare/Q4.java
+++ src/main/java/workbook2/methodcompare/Q4.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 class StringUtil {
     static boolean isPalindrome(String s) {
-        // TODO: 회문 여부 판단
-        return false;
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+            return true;
+
+//      String reversed = new StringBuilder(s).reverse().toString();
+//      return s.equals(reversed);
     }
 }
 
@@ -13,6 +21,8 @@ public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        // TODO: StringUtil.isPalindrome 호출 후 출력
+
+        boolean isPalindrome = StringUtil.isPalindrome(str);
+        System.out.println(isPalindrome);
     }
 }
