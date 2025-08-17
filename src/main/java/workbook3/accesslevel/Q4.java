6755e7ec09ab5f3fefdd41dd800dce4172a0e624
hailee2 <slowslow9150@gmail.com>
2025-08-17 13:42:16 +0900

    feat: [accesslevel] Q4 구현

--- src/main/java/workbook3/accesslevel/Q4.java
+++ src/main/java/workbook3/accesslevel/Q4.java
@@ -3,18 +3,20 @@ package main.java.workbook3.accesslevel;
 import java.util.Scanner;
 
 class LibraryBook {
+    //private 변수
     private String title;
     private String author;
 
+    //생성자
     LibraryBook(String title, String author) {
         this.title = title;
         this.author = author;
     }
-
+    //제목 getter 메서드
     public String getTitle() {
         return title;
     }
-
+    //작가 getter 메서드
     public String getAuthor() {
         return author;
     }
@@ -25,12 +27,14 @@ public class Q4 {
         Scanner sc = new Scanner(System.in);
         LibraryBook[] books = new LibraryBook[3];
         for (int i = 0; i < 3; i++) {
-            String t = sc.next();
-            String a = sc.next();
+            String t = sc.next();               //책 제목 입력
+            String a = sc.next();               //저자 입력
             books[i] = new LibraryBook(t, a);
         }
         for (LibraryBook b : books) {
             System.out.println(b.getTitle() + " " + b.getAuthor());
         }
+
+        sc.close();
     }
 }
