cb16acd5b82798eff9b47e1707f3a723602d0b41
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-17 14:01:47 +0900

    varscope Q4 구현

--- src/main/java/workbook2/varscope/Q4.java
+++ src/main/java/workbook2/varscope/Q4.java
@@ -2,16 +2,18 @@ package main.java.workbook2.varscope;
 
 public class Q4 {
     static {
-        System.out.println("static block");
+        System.out.println("static block"); // 최초 실행 1번만 출력
     }
     {
-        System.out.println("instance block");
+        System.out.println("instance block"); // 인스턴스 초기화 블록 출력
     }
     Q4() {
-        System.out.println("constructor");
+        System.out.println("constructor"); // 생성자 출력
     }
 
     public static void main(String[] args) {
         Q4 d = new Q4();
+        // 차이를 비교하기 위해 하나 더 생성
+        Q4 d2 = new Q4();
     }
 }
