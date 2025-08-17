514d3cc4b24d6d177fc1f9bf9042b9b8e4462954
hailee2 <slowslow9150@gmail.com>
2025-08-17 15:40:56 +0900

    feat: [abstractinterface] Q4 구현

--- src/main/java/workbook3/abstractinterface/Q4.java
+++ src/main/java/workbook3/abstractinterface/Q4.java
@@ -1,20 +1,23 @@
 package main.java.workbook3.abstractinterface;
 
 import java.util.Scanner;
-
+//Movable 인터페이스
 interface Movable {
+    //모든 Movable을 implements한 클래스는 move()메서드를 구현해야함
     void move();
 }
-
+//인터페이스 Movable를 implements한 Robot 클래스
 class Robot implements Movable {
     public void move() {
         // TODO: "Robot moves" 출력
+        System.out.println("Robot moves");
     }
 }
-
+//인터페이스 Movable를 implements한 Car 클래스
 class Car implements Movable {
     public void move() {
         // TODO: "Car moves" 출력
+        System.out.println("Car moves");
     }
 }
 
@@ -29,5 +32,6 @@ public class Q4 {
         for (Movable m : objs) {
             m.move();
         }
+        sc.close();
     }
 }
