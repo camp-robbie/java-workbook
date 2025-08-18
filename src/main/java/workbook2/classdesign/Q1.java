19f26f74a7f6f6e220a73de0dd342481efdbcffc
zerone <bazz0324@naver.com>
2025-08-18 17:47:31 +0900

    "feat: [classdesign] Q1 구현"

--- src/main/java/workbook2/classdesign/Q1.java
+++ src/main/java/workbook2/classdesign/Q1.java
@@ -2,13 +2,19 @@ package main.java.workbook2.classdesign;
 
 import java.util.Scanner;
 
+// Rectangle 클래스를 설계하여 가로(width)와 세로(height)를 필드로 가지고, 넓이를 반환하는 메서드 getArea()를 작성하시오.
 class Rectangle {
     int width;
     int height;
     // TODO: 생성자 작성
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     int getArea() {
         // TODO: 넓이 반환
-        return 0;
+        return  width * height;
     }
 }
 
@@ -17,7 +23,9 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
         int h = sc.nextInt();
-        // Rectangle rect = new Rectangle(w, h); // TODO: 생성자 작성 후 주석 해제
+         Rectangle rect = new Rectangle(w, h); // TODO: 생성자 작성 후 주석 해제
+
         // TODO: rect.getArea() 출력
+        System.out.println(rect.getArea());
     }
 }
