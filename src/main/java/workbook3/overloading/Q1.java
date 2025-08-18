053a26f8ce05256c3b0cbad20d3947bb5bcf4be5
HY-WG <mung94c@gmail.com>
2025-08-18 20:23:02 +0900

    overloading solutions

--- src/main/java/workbook3/overloading/Q1.java
+++ src/main/java/workbook3/overloading/Q1.java
@@ -4,14 +4,28 @@ import java.util.Scanner;
 
 public class Q1 {
     // TODO: printValue 오버로딩 (int), (double), (String)
+    //printValue 메서드를 정수, 실수, 문자열 세 가지 매개변수 타입에 대해 오버로딩하여 각각 값을 출력하는 프로그램을 작성하시오.
+
+    static void printValue(int x) {
+        System.out.println(x);
+    }
+
+    static void printValue(double x) {
+        System.out.println(x);
+    }
+
+    static void printValue(String x) {
+        System.out.println(x);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
         double d = sc.nextDouble();
         String s = sc.next();
         // TODO: printValue (int), (double), (String) 메서드 작성 후 아래 주석 해제
-        // printValue(i);
-        // printValue(d);
-        // printValue(s);
+         printValue(i);
+         printValue(d);
+         printValue(s);
     }
 }
--- src/main/java/workbook3/overloading/Q5.java
+++ src/main/java/workbook3/overloading/Q5.java
@@ -4,13 +4,26 @@ import java.util.Scanner;
 
 public class Q5 {
     // TODO: format 오버로딩 (int), (String), (String,int)
+
+    static String format(int num) {
+        return "정수:" + num;
+    }
+
+    static String format(String s) {
+        return "문자열: " + s;
+    }
+
+    static String format(String name, int age) {
+        return name + "(" + age + ")";
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         int age = sc.nextInt();
         // TODO: format 메서드 작성 후 아래 주석 해제
-        // System.out.println(format(name));
-        // System.out.println(format(age));
-        // System.out.println(format(name, age));
+         System.out.println(format(name));
+         System.out.println(format(age));
+         System.out.println(format(name, age));
     }
 }
