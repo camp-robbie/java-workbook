b92f88d085d112afaa4e979a1c4bb5189a72d803
Xellos216 <xellos216@gmail.com>
2025-08-20 01:00:17 +0900

    Solve overloading

--- src/main/java/workbook3/overloading/Q4.java
+++ src/main/java/workbook3/overloading/Q4.java
@@ -3,21 +3,35 @@ package main.java.workbook3.overloading;
 import java.util.Scanner;
 
 public class Q4 {
-    // TODO: area 오버로딩 (double r), (double w, double h), (double base, double height)
+    public static double area(double r) {
+        return 2 * Math.PI * r;
+    }
+
+    public static double area(double w, double h) {
+        return w * h;
+    }
+
+    public static double area(double base, double height, boolean isTriangle) {
+        return base * height * 0.5;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String type = sc.next();
-        if (type.equals("circle")) {
-            double r = sc.nextDouble();
-            // System.out.println(area(r)); // TODO: area 메서드 작성 후 주석 해제
-        } else if (type.equals("rect")) {
-            double w = sc.nextDouble();
-            double h = sc.nextDouble();
-            // System.out.println(area(w,h)); // TODO: area 메서드 작성 후 주석 해제
-        } else {
-            double b = sc.nextDouble();
-            double h = sc.nextDouble();
-            // System.out.println(area(b,h)); // TODO: area 메서드 작성 후 주석 해제
+        for (int i = 0; i < 3; i++) {
+            String type = sc.next();
+            if (type.equals("circle")) {
+                double r = sc.nextDouble();
+                System.out.println(area(r));
+            } else if (type.equals("rect")) {
+                double w = sc.nextDouble();
+                double h = sc.nextDouble();
+                System.out.println(area(w, h));
+            } else {
+                double b = sc.nextDouble();
+                double h = sc.nextDouble();
+                System.out.println(area(b, h, true));
+            }
         }
     }
 }
